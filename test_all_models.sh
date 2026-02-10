b1a75ffff0d329e49dce2a42d5ec138564669a76
Zak Cory <zakhrik@gmail.com>
2026-02-10 11:08:11 +0200

    more fixes

--- test_all_models.sh
+++ test_all_models.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo $"\n\n==================Starting TensorRT testing...==================\n\n"
+echo -e "\n\n==================Starting TensorRT testing...==================\n\n"
 
 COMPOSE_FILE="docker-compose-triton.yml"
 
 docker compose -f $COMPOSE_FILE run --pull always --rm -v "./:/tmp" --remove-orphans triton_server bash -lc '
 cd /tmp
 
-echo $"\n\n==================Testing the 512 optimized engine==================\n\n"
+echo -e "\n\n==================Testing the 512 optimized engine==================\n\n"
 /usr/src/tensorrt/bin/trtexec --loadEngine=sources/models/trt/dinov3_vitb16-512.engine --shapes=images:1x3x512x512 --dumpOutput
 
-echo $"\n\n==================Testing the 112 optimized engine==================\n\n"
+echo -e "\n\n==================Testing the 112 optimized engine==================\n\n"
 /usr/src/tensorrt/bin/trtexec --loadEngine=sources/models/trt/dinov3_vitb16-112.engine --shapes=images:1x3x224x224 --dumpOutput
 '
